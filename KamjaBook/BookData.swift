520bae717f0936a02f1a50ffc05586872fd7f665
SEO_MAC <seo_mac@SEO-MACui-MacBook-Pro.local>
2017-03-06 01:22:37 +0900

    list 구현

---
+++ KamjaBook/BookData.swift
@@ -0,0 +1,42 @@
+//
+//  BookData.swift
+//  KamjaBook
+//
+//  Created by SEO_MAC on 2017. 3. 6..
+//  Copyright © 2017년 SEO_MAC. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class BookData {
+    var title = ""
+    var link = ""
+    var image = ""
+    var author = ""
+    var price = ""
+    var discount = ""
+    var publisher = ""
+    var pubdate = ""
+    var isbn = ""
+    var description = ""
+    /*
+    func insert(obj : AnyObject ){
+        self.link = ""
+        
+        let  json  =  JSON ( obj )
+        let total = json["total"]
+        
+        //debugPrint(json)
+        //debugPrint("total: \(total)")
+        
+        
+        for item in json["items"].arrayValue{
+            debugPrint(item["title"])
+        }
+        
+
+    }
+ */
+    
+}
--- KamjaBook/ViewController.swift
+++ KamjaBook/ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController ,UITableViewDataSource{
 
     @IBOutlet var tableView: UITableView!
     
-    let bookList = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    var bookData = [BookData]()
+    
+    //let bookList = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
     let cellReuseIdentifier = "booklistcell"
     
     override func viewDidLoad() {
@@ -27,53 +29,22 @@ class ViewController: UIViewController ,UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.bookList.count
+        return self.bookData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 첫 번째 인자로 등록한 identifier, cell은 as 키워드로 앞서 만든 custom cell class화 해준다. 
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! BookListCell
 
-        cell.titleLabel?.text = bookList[indexPath.row]
-        
+        cell.titleLabel?.text = bookData[indexPath.row].title
+        cell.subTitleLabel?.text = bookData[indexPath.row].author
+        cell.authorLabel?.text = bookData[indexPath.row].pubdate
         return cell
     }
   
     
     func initRes(){
         print("init...........")
-        
-        /*
-        let request = Alamofire.request("https://openapi.naver.com/v1/search/book.json?query=%EC%A3%BC%EC%8B%9D&display=10&start=1", headers: headers)
-            .responseJSON { response in
-                debugPrint("<<<res strat====================================================")
-                debugPrint(response)
-                
-                //let str = String(UTF8String: response.cStringUsingEncoding(String.Encoding.utf8))
-                
-                debugPrint("<<<res end====================================================")
-
-        }
-        debugPrint(">>>req start====================================================")
-        debugPrint(request)
-        debugPrint(">>>req end====================================================")
-        */
-      /*
-        let request = Alamofire.request( "https://openapi.naver.com/v1/search/book.json?query=%EC%A3%BC%EC%8B%9D&display=10&start=1", headers: headers)
-            .responseString(encoding: String.Encoding.utf8) { ( response) -> Void in
-               
-            debugPrint("<<<res strat====================================================")
-            let statusCode = (response.response?.statusCode)!
-            debugPrint("status: \(statusCode)")
-            debugPrint(response)
-            debugPrint("<<<res end====================================================")
-                
-        }
-        debugPrint(">>>req start====================================================")
-        debugPrint(request)
-        debugPrint(">>>req end====================================================")
-        */
-        
   
         let sessionManager = Alamofire.SessionManager.default
         sessionManager.adapter = MashapeHeadersAdapter()
@@ -83,45 +54,51 @@ class ViewController: UIViewController ,UITableViewDataSource{
             //.responseString( encoding: String.Encoding.utf8 ){( response) -> Void in
             .responseJSON { response in
             debugPrint("<<<res strat====================================================")
+
             let statusCode = (response.response?.statusCode)!
             debugPrint("status: \(statusCode)")
             debugPrint(type(of: response))
                 
-            //debugPrint(response)
-            //self.parseJSONResults( jsonData: String( describing: response) )
-            
             guard let object = response.result.value else {
                 print("Oh, no!!!")
                 return
             }
-            
-            /*
-            guard let object = response.result.value as? [[String: AnyObject]] else {
-                print("didn't get todo objects as JSON from API")
-                    //completionHandler(.failure(BackendError.objectSerialization(reason: "Did not get JSON array in response")))
-                return
-            }
-            */
-            //let json = JSON(object)
-               
+            //debugPrint(type(of: object))
+            //debugPrint(object)
+         
             let  json  =  JSON ( object )
+
             let total = json["total"]
                 
+            //debugPrint(json)
             debugPrint("total: \(total)")
                 
-            
             for item in json["items"].arrayValue{
-                debugPrint(item["title"])
+                //debugPrint(item["title"])
+                
+                let book = BookData()
+                book.title = item["title"].string!
+                book.author =  item["author"].string!
+                book.link =  item["link"].string!
+                book.image =  item["image"].string!
+                book.price =  item["price"].string!
+                book.discount =  item["discount"].string!
+                book.publisher =  item["publisher"].string!
+                book.pubdate =  item["pubdate"].string!
+                book.isbn =  item["isbn"].string!
+                book.description =  item["description"].string!
+                
+                self.bookData.append(book)
             }
                 
-            debugPrint(object)
-         
+            debugPrint(self.bookData.count)
+                
+            //let book = BookData()
+            //book.insert(obj: object as AnyObject)
                 
             debugPrint("<<<res end====================================================")
-            
-            //.responseJSON { response in
-            //    debugPrint("res================================")
-            //    debugPrint(response)
+                
+            self.tableView.reloadData()
         }
        
         debugPrint(request)
@@ -129,41 +106,8 @@ class ViewController: UIViewController ,UITableViewDataSource{
     }
     
     
-    func parseJSONResults(jsonData value:String ) {
-        //var json
-        do {
-            let data = value.data(using: .utf8)!
-            if  value != "" {
-              //  let json = try JSONSerialization.jsonObject(with: value, options: []) as! [String: AnyObject]
-                let allContacts = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String : AnyObject]
-                if let arrJSON = allContacts["items"] {
-                    for index in 0...arrJSON.count-1 {
-                        let aObject = arrJSON[index] as! [String : AnyObject]
-                        //names.append(aObject["title"] as! String)
-                        //contacts.append(aObject["author"] as! String)
-                        //contacts.append(aObject["publisher"] as! String)
-                        var title = aObject["title"] as! String
-                        debugPrint(title)
-
-                    }
-                }
-                //return items
-            } else {
-                print("No Data :/")
-            }
-        } catch {
-            // 실패한 경우, 오류 메시지를 출력합니다.
-            print("Error, Could not parse the JSON request")
-        }
-        
-       // return nil
-    }
     
     
-    func reqBookSearc(){
-        
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
